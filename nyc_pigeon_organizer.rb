9e4808d0e77e8d83b5a5144fd0c2dd15e4fa0453
Drew Price <drewprice@users.noreply.github.com>
2015-08-02 18:02:59 -0400

    Add solution

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names = data[:gender].values.flatten
+
+  names.each_with_object({}) do |name, pigeons_by_name|
+    pigeons_by_name[name] = {}
+
+    data.each_pair do |feature, options|
+      pigeons_by_name[name][feature] = []
+
+      options.each_pair do |option, pigeons|
+        pigeons_by_name[name][feature] << option.to_s if pigeons.include? name
+      end
+    end
+  end
 end
